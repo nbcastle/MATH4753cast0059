5284f8d35d5fa9a7fbbbd74731232721b7948eba
Admin <Admin@COE-NEL119-CN.sooner.net.ou.edu>
2022-01-19 11:16:47 -0600

    First Class

---
+++ R/mygetwd.R
@@ -0,0 +1,13 @@
+#' @title This finds the working directory
+#'
+#' @description This is where we put more stuff
+#'
+#' @return working directory
+#' @export
+#'
+#' @examples mygetwd()
+mygetwd <- function(){
+  getwd()
+}
+
+mygetwd()

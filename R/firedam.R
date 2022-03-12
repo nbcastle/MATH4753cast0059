b15e9e8510bedc0ee9b4a2a22cc6c9c98360bfa0
Admin <Admin@COE-NEL119-CN.sooner.net.ou.edu>
2022-03-11 19:11:57 -0600

    completed lab 7, added firedam

---
+++ R/firedam.R
@@ -0,0 +1,12 @@
+#' Distance and damage from fire
+#'
+#' A dataset containing 15 samples of the distance and damage of fire
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance of fire}
+#'   \item{DAMAGE}{damage of fire}
+#'   ...
+#' }
+#' @source \url{MATH4753 files}
+"firedam"
